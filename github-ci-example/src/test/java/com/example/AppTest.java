81b9bebb381fee4e34abd15ed2afd1bc927e10d3
Massimiliano Mancini <massimiliano1.mancini@gmail.com>
2021-08-10 13:56:34 +0200

    First commit

---
+++ github-ci-example/src/test/java/com/example/AppTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+
+	private App app;
+	
+	@BeforeEach
+	void setup() {
+		app = new App();
+	}
+	
+	@Test
+    public void testSayHello()
+    {
+        assertThat(app.sayHello()).isEqualTo("Hello");
+    }
+}
